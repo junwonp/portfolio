6df0e0dc878faed96cdb6f2790e48d12416a94ea
Junwon <26790365+junwonp@users.noreply.github.com>
2026-03-27 18:50:31 +0900

    perf: enable precompress for Brotli/gzip static assets

--- svelte.config.js
+++ svelte.config.js
@@ -34,7 +34,7 @@ const config = {
       pages: 'build',
       assets: 'build',
       fallback: '404.html',
-      precompress: false,
+      precompress: true,
       strict: true,
     }),
     alias: {
